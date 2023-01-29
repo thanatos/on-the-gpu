60667a26856dae4968d467f022e732219f944bdc
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2023-01-28 23:37:23 -0500

    Switch to clap for argument parsing
    Little, apparently, did I know that `structopt` has been deprecated,
    apparently subsumed by clap.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
 use std::ffi::{CString, OsStr, OsString};
 use std::os::unix::ffi::OsStrExt;
 
-use structopt::StructOpt;
+use clap::Parser;
 
-#[derive(StructOpt)]
+/// Run a program on the (discrete) GPU.
+#[derive(Parser)]
 struct Args {
     // TODO: this is going to be used for logging, at some point.
+    /// A name for the game, this is used to build the filename for the log file.
     game_name: String,
-    rest: Vec<OsString>,
+    /// The arguments of the command to run, including the binary. E.g., `space-game --aliens`.
+    command: Vec<OsString>,
 }
 
 fn main() {
-    let args = Args::from_args();
-    if args.rest.is_empty() {
+    let args = Args::parse();
+    if args.command.is_empty() {
         eprintln!("Need at least 1 argument for the command to run.");
         std::process::exit(1);
     }
     println!("== Start ==");
     println!("CWD: {:?}", std::env::current_dir());
     println!("Arguments:");
-    for (idx, arg) in args.rest.iter().enumerate() {
+    for (idx, arg) in args.command.iter().enumerate() {
         println!("  argv[{}] = {:?}", idx, arg);
     }
 
     let mut to_exec_args = Vec::<CString>::new();
     to_exec_args.push(string_to_cstring("pvkrun".to_owned()));
-    for arg in args.rest.iter() {
+    for arg in args.command.iter() {
         to_exec_args.push(os_str_to_cstring(arg));
     }
     nix::unistd::execvp(&to_exec_args[0], &to_exec_args).unwrap();
